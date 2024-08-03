e85f1ba5b1ecefbcd2078b55dd3580a9f8846981
eloquentarduino <eloquentarduino@gmail.com>
2024-08-03 15:31:01 +0200

    bump dist to 2.7.13

--- src/eloquent_esp32cam/camera/pinout.h
+++ src/eloquent_esp32cam/camera/pinout.h
@@ -1,6 +1,8 @@
 #ifndef ELOQUENT_ESP32CAM_CAMERA_PINOUT
 #define ELOQUENT_ESP32CAM_CAMERA_PINOUT
 
+#include <Preferences.h>
+#include <esp_camera.h>
 
 namespace Eloquent {
     namespace Esp32cam {
@@ -40,7 +42,7 @@ namespace Eloquent {
                 /**
                  *
                  */
-                void aithinker() {
+                bool aithinker() {
                     pins.d0 = 5;
                     pins.d1 = 18;
                     pins.d2 = 19;
@@ -58,12 +60,13 @@ namespace Eloquent {
                     pins.pwdn = 32;
                     pins.reset = -1;
                     pins.flashlight = 4;
+                    return true;
                 }
 
                 /**
                  *
                  */
-                void m5() {
+                bool m5() {
                     pins.d0 = 32;
                     pins.d1 = 35;
                     pins.d2 = 34;
@@ -81,12 +84,13 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = 15;
                     pins.flashlight = 16;
+                    return true;
                 }
 
                 /**
                  *
                  */
-                void m5_wide() {
+                bool m5_wide() {
                     pins.d0 = 32;
                     pins.d1 = 35;
                     pins.d2 = 34;
@@ -104,12 +108,13 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = 15;
                     pins.flashlight = 14;
+                    return true;
                 }
 
                 /**
                  *
                  */
-                void eye() {
+                bool eye() {
                     pins.d0 = 34;
                     pins.d1 = 13;
                     pins.d2 = 14;
@@ -127,12 +132,13 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = -1;
                     pins.flashlight = 22;
+                    return true;
                 }
 
                 /**
                  *
                  */
-                void wrover() {
+                bool wrover() {
                     pins.d0 = 4;
                     pins.d1 = 5;
                     pins.d2 = 18;
@@ -150,12 +156,13 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = -1;
                     pins.flashlight = -1;
+                    return true;
                 }
 
                 /**
                  *
                  */
-                void wroom_s3() {
+                bool wroom_s3() {
                     pins.d0 = 11;
                     pins.d1 = 9;
                     pins.d2 = 8;
@@ -172,20 +179,21 @@ namespace Eloquent {
                     pins.sccb_scl = 5;
                     pins.pwdn = -1;
                     pins.reset = -1;
-                    pins.flashlight = -1; 
+                    pins.flashlight = -1;
+                    return true;
                 }
 
                 /**
                  * 
                  */
-                void freenove_s3() {
-                    wroom_s3();
+                bool freenove_s3() {
+                    return wroom_s3();
                 }
 
                 /**
                  *
                  */
-                void ttgo_lcd() {
+                bool ttgo_lcd() {
                     pins.d0 = 34;
                     pins.d1 = 13;
                     pins.d2 = 26;
@@ -203,12 +211,13 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = -1;
                     pins.flashlight = -1;
+                    return true;
                 }
 
                 /**
                  *
                  */
-                void ttgo_pir() {
+                bool ttgo_pir() {
                     pins.d0 = 5;
                     pins.d1 = 14;
                     pins.d2 = 4;
@@ -226,12 +235,13 @@ namespace Eloquent {
                     pins.pwdn = 26;
                     pins.reset = -1;
                     pins.flashlight = -1;
+                    return true;
                 }
 
                 /**
                  *
                  */
-                void simcam() {
+                bool simcam() {
                     pins.d0 = 11;
                     pins.d1 = 9;
                     pins.d2 = 8;
@@ -249,12 +259,13 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = 18;
                     pins.flashlight = -1;
+                    return true;
                 }
 
                 /**
                  * 
                  */
-                void xiao() {
+                bool xiao() {
                     pins.d0 = 15;
                     pins.d1 = 17;
                     pins.d2 = 18;
@@ -272,12 +283,13 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = -1;
                     pins.flashlight = -1;
+                    return true;
                 }
 
                 /**
                  * 
                  */
-                void m5_timer() {
+                bool m5_timer() {
                     pins.d0 = 32;
                     pins.d1 = 35;
                     pins.d2 = 34;
@@ -295,12 +307,13 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = 15;
                     pins.flashlight = 2;
+                    return true;
                 }
 
                 /**
                  *
                  */
-                void lilygo_tcamera_s3() {
+                bool lilygo_tcamera_s3() {
                     pins.d0 = 14;
                     pins.d1 = 47;
                     pins.d2 = 48;
@@ -318,29 +331,14 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = 39;
                     pins.flashlight = -1;
+                    return true;
                 }
 
-                void eye_s3() {
-                    pins.d0 = 11;
-                    pins.d1 = 9;
-                    pins.d2 = 8;
-                    pins.d3 = 10;
-                    pins.d4 = 12;
-                    pins.d5 = 18;
-                    pins.d6 = 17;
-                    pins.d7 = 16;
-                    pins.xclk = 15;
-                    pins.pclk = 13;
-                    pins.vsync = 6;
-                    pins.href = 7;
-                    pins.sccb_sda = 4;
-                    pins.sccb_scl = 5;
-                    pins.pwdn = -1;
-                    pins.reset = -1;
-                    pins.flashlight = -1;
+                bool eye_s3() {
+                    return wroom_s3();
                 }
 
-                void kaluga() {
+                bool kaluga() {
                     pins.d0 = 36;
                     pins.d1 = 37;
                     pins.d2 = 41;
@@ -358,6 +356,110 @@ namespace Eloquent {
                     pins.pwdn = -1;
                     pins.reset = -1;
                     pins.flashlight = -1;
+                    return true;
+                }
+
+                /**
+                 * Automatically detect model
+                 */
+                bool autodetect() {
+                    preferences.begin("pinout:1", false);
+
+                    bool detected = false
+                        || aithinker() && test("aithinker")
+                        || wroom_s3() && test("wroom_s3")
+                        || xiao() && test("xiao")
+                        || m5() && test("M5")
+                        || m5_wide() && test("M5 wide")
+                        || m5_timer() && test("M5 timer")
+                        || eye() && test("ESP-EYE")
+                        || eye_s3() && test("ESP-EYE S3")
+                        || wrover() && test("Wrover")
+                        || ttgo_lcd() && test("TTGO LCD")
+                        || ttgo_pir() && test("TTGO PIR")
+                        || lilygo_tcamera_s3() && test("T-CAMERA S3")
+                        || simcam() && test("SIMCAM")
+                        ;
+
+                    preferences.end();
+
+                    return detected;
+                }
+
+            protected:
+                Preferences preferences;
+
+                /**
+                 * Test current configuration
+                 * @return
+                 */
+                bool test(const char *model) {
+                    ESP_LOGI("Pinout", "Testing pinout for model %s", model);
+                    int8_t existing = preferences.getChar(model, 0);
+
+                    // since a wrong config may cause a restart,
+                    // mark a failure by default
+                    preferences.putChar(model, -1);
+
+                    // 1 is for correct config
+                    if (existing == 1)
+                        return true;
+
+                    // -1 is for wrong config
+                    if (existing == -1)
+                        return false;
+
+                    // new config, try it
+                    camera_config_t config;
+
+                    config.pin_d0 = pins.d0;
+                    config.pin_d1 = pins.d1;
+                    config.pin_d2 = pins.d2;
+                    config.pin_d3 = pins.d3;
+                    config.pin_d4 = pins.d4;
+                    config.pin_d5 = pins.d5;
+                    config.pin_d6 = pins.d6;
+                    config.pin_d7 = pins.d7;
+                    config.pin_xclk = pins.xclk;
+                    config.pin_pclk = pins.pclk;
+                    config.pin_vsync = pins.vsync;
+                    config.pin_href = pins.href;
+                    config.pin_sccb_sda = pins.sccb_sda;
+                    config.pin_sccb_scl = pins.sccb_scl;
+                    config.pin_pwdn = -1;
+                    config.pin_reset = -1;
+
+                    config.ledc_channel = LEDC_CHANNEL_0;
+                    config.ledc_timer = LEDC_TIMER_0;
+                    config.fb_count = 1;
+                    config.frame_size = FRAMESIZE_QQVGA;
+                    config.pixel_format = PIXFORMAT_JPEG;
+                    config.jpeg_quality = 10;
+                    config.xclk_freq_hz = 10000000ULL;
+                    config.grab_mode = CAMERA_GRAB_LATEST;
+
+                    camera_fb_t *frame;
+
+                    if (esp_camera_init(&config) == ESP_OK) {
+                        ESP_LOGI("Pinout", "Init ok! Trying to capture frame...");
+                        frame = esp_camera_fb_get();
+
+                        if (frame != NULL && frame->len > 0) {
+                            ESP_LOGI("Pinout", "Match found!");
+                            preferences.putChar(model, 1);
+                            esp_camera_fb_return(frame);
+                            esp_camera_deinit();
+                            return true;
+                        }
+                        else
+                            ESP_LOGI("Pinout", "Cannot capture frame, moving to next model...");
+                    }
+
+                    if (frame != NULL)
+                        esp_camera_fb_return(frame);
+
+                    esp_camera_deinit();
+                    return false;
                 }
             };
         }
